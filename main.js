98f1cf89bf32e5068a2824ea1db5270a738feb07
Pavlos Vasileiadis <PhotoPaul@gmail.com>
2023-12-29 09:09:37 +0200

    key.algorithm does not match that of operation

--- main.js
+++ main.js
@@ -1,5 +1,73 @@
-function encrypt() {
-  let keyPair = generateKeyPair();
+async function encrypt() {
+  // let keyPair = generateKeyPair();
+  let publicKeys = await fetchPublicKeys();
+  // console.log(publicKeys.proxyPublicKey);
+  // console.log(publicKeys.resolverPublicKey);
+  publicKeys.proxyPublicKeyImported = await pemPublicKeyToWebKey(publicKeys.proxyPublicKey);
+  publicKeys.resolverPublicKeyImported = await pemPublicKeyToWebKey(publicKeys.resolverPublicKey);
+  debugger;
+  let response = await encryptMessage(publicKeys.proxyPublicKeyImported, "Hello World!");
+}
+
+async function encryptMessage(publicKey, message) {
+  // Convert the message to an ArrayBuffer
+  const encoder = new TextEncoder();
+  const data = encoder.encode(message);
+  
+  // Generate a random initialization vector
+  const iv = window.crypto.getRandomValues(new Uint8Array(12));
+  
+  // Encrypt the data
+  const encryptedData = await window.crypto.subtle.encrypt(
+      {
+          name: "AES-GCM",
+          iv: iv
+      },
+      publicKey,
+      data
+  );
+  
+  return { iv, encryptedData };
+}
+
+function pemPublicKeyToWebKey(pemPublicKey) {
+  // Fetch the part of the PEM string between header and footer
+  const pemHeader = "-----BEGIN PUBLIC KEY-----";
+  const pemFooter = "-----END PUBLIC KEY-----";
+  const pemContents = pemPublicKey.substring(pemHeader.length, pemPublicKey.length - pemFooter.length);
+  
+  // Base64 decode the string to get the binary data
+  const binaryDerString = window.atob(pemContents);
+  
+  // Convert from a binary string to an ArrayBuffer
+  const binaryDer = stringToArrayBuffer(binaryDerString);
+  
+  return window.crypto.subtle.importKey(
+      "spki",
+      binaryDer,
+      {
+          name: "RSASSA-PKCS1-v1_5",
+          hash: "SHA-256",
+      },
+      true,
+      ["verify"]
+  );
+}
+
+// Function to convert string to ArrayBuffer
+function stringToArrayBuffer(str) {
+  const buf = new ArrayBuffer(str.length);
+  const bufView = new Uint8Array(buf);
+  for (let i = 0, strLen = str.length; i < strLen; i++) {
+      bufView[i] = str.charCodeAt(i);
+  }
+  return buf;
+}
+
+async function fetchPublicKeys() {
+  const response = await fetch('https://script.google.com/macros/s/AKfycbx5-apWsu9ZWPtneyW7oNzLMsutDzG7_JtHCew2wEcacYvWjwCjp3okOFwp2YdqTU0T/exec'); // Replace with your URL
+  const publicKeys = await response.json();
+  return publicKeys;
 }
 
 async function generateKeyPair() {
@@ -20,7 +88,7 @@ async function webCryptoPublicKeyToPEM(publicKey) {
     .then(keyData => {
       const keyString = arrayBufferToString(keyData);
       const base64Key = window.btoa(keyString);
-      const pemKey = formatAsPem(base64Key);
+      const pemKey = formatPublicKeyAsPem(base64Key);
       return pemKey;
     });
 }
@@ -30,7 +98,7 @@ async function webCryptoPrivateKeyToPEM(privateKey) {
     .then(keyData => {
       const keyString = arrayBufferToString(keyData);
       const base64Key = window.btoa(keyString);
-      const pemKey = formatAsPem(base64Key);
+      const pemKey = formatPrivateKeyAsPem(base64Key);
       return pemKey;
     });
 }
@@ -44,12 +112,22 @@ function arrayBufferToString(buffer) {
   return binary;
 }
 
-function formatAsPem(str) {
+function formatPublicKeyAsPem(str) {
   let finalString = '-----BEGIN PUBLIC KEY-----\n';
   while (str.length > 0) {
     finalString += str.substring(0, 64) + '\n';
     str = str.substring(64);
   }
   finalString += "-----END PUBLIC KEY-----";
+  return finalString;
+}
+
+function formatPrivateKeyAsPem(str) {
+  let finalString = '-----BEGIN RSA PRIVATE KEY-----\n';
+  while (str.length > 0) {
+    finalString += str.substring(0, 64) + '\n';
+    str = str.substring(64);
+  }
+  finalString += "-----END RSA PRIVATE KEY-----";
   return finalString;
 }
